fd6a8569fa530ab8f173fd2c9c7eca39d82cd8d5
Khatantuul Batbold <batbold.k@northeastern.edu>
2024-02-24 21:59:45 -0500

    Fixing variables

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ resource "google_compute_network" "vpc" {
   for_each = var.all_vpcs
 
   name                    = each.value.name
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
-  delete_default_routes_on_create = true
+  auto_create_subnetworks = each.value.autocreatesubnetworks
+  routing_mode            = each.value.routingmode
+  delete_default_routes_on_create = each.value.deletedefaultroutesoncreate
 }
 
 resource "google_compute_subnetwork" "webapp-subnet" {
@@ -33,25 +33,25 @@ resource "google_compute_subnetwork" "db-subnet" {
 resource "google_compute_route" "webapp_route" {
   for_each = var.all_vpcs
 
-  name                = "webapp-route"
+  name                = each.value.webapproute
   network             = google_compute_network.vpc[each.key].self_link
-  dest_range  = "0.0.0.0/0"
-  next_hop_gateway    = "default-internet-gateway" 
+  dest_range  = each.value.webapproute_dest_range
+  next_hop_gateway    = each.value.webapp_next_hop_gateway
   tags = ["route-webapp"]
 }
 
 resource "google_compute_firewall" "allow_traffic" {
   for_each = var.all_vpcs
 
-  name    = "testt"
+  name    = each.value.firewall_allow_name
   network = google_compute_network.vpc[each.key].self_link
   priority = 1000
-  direction = "INGRESS"
+  direction = each.value.firewall_allow_direction
   allow {
     protocol = "tcp"
-    ports = ["8080","80"]
+    ports = each.value.firewall_allow_ports
   }
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = each.value.firewall_source_ranges
   target_tags = ["route-webapp"]
   depends_on = [google_compute_network.vpc]
   
@@ -60,14 +60,14 @@ resource "google_compute_firewall" "allow_traffic" {
 resource "google_compute_firewall" "deny_ssh_login" {
   for_each = var.all_vpcs
 
-  name    = "denial"
+  name    = each.value.firewall_deny_name
   network = google_compute_network.vpc[each.key].self_link
   
    deny {
     protocol = "tcp"
-    ports    = ["20"]
+    ports    = each.value.firewall_deny_ports
   }
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = each.value.firewall_source_ranges
   target_tags = ["route-webapp"]
   depends_on = [google_compute_network.vpc]
   
@@ -77,8 +77,8 @@ resource "google_compute_instance" "my-instance" {
   for_each = var.all_vpcs
 
   name         = "my-instance-${each.key}"
-  machine_type = "e2-medium"  
-  zone         = "us-east1-b"
+  machine_type = each.value.vm_machine_type 
+  zone         = each.value.vm_zone
 
   boot_disk {
     initialize_params {
@@ -86,13 +86,13 @@ resource "google_compute_instance" "my-instance" {
       size = each.value.size
       type = each.value.disktype
     }
-    mode = "READ_WRITE"
+    mode = each.value.vm_disk_mode
   }
 
   
   network_interface {
     access_config {
-      network_tier = "PREMIUM"
+      network_tier = each.value.vm_networking_tier
     }
    subnetwork = google_compute_subnetwork.webapp-subnet[each.key].id
 
--- variables.tf
+++ variables.tf
@@ -2,21 +2,39 @@
 
 variable "project_id" {
   description = "Google Cloud project ID"
+  type = string
 }
 
 variable "region" {
   description = "GCP region"
+  type = string 
 }
 
 variable "all_vpcs" {
   type = map(object({
     name            = string
+    autocreatesubnetworks = bool
+    routingmode = string
+    deletedefaultroutesoncreate = bool 
     webappsubnet       = string
     webappsubnetcidr  = string
+    webapproute = string
+    webapproute_dest_range = string
+    webapp_next_hop_gateway = string
     dbsubnet           = string
     dbsubnetcidr      = string
+    firewall_allow_name = string
+    firewall_allow_direction = string
+    firewall_allow_ports = list(string)
+    firewall_source_ranges = list(string)
+    firewall_deny_name = string
+    firewall_deny_ports = list(string)
     image = string
     size = string
     disktype = string
+    vm_zone = string
+    vm_machine_type = string
+    vm_networking_tier = string
+    vm_disk_mode = string
   }))
 }
