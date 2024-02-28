ab1088bb2c258c6e948463c0a549155ec52d9518
Khatantuul Batbold <batbold.k@northeastern.edu>
2024-02-27 22:17:20 -0500

    Adding assignment 5

--- main.tf
+++ main.tf
@@ -1,29 +1,31 @@
 # main.tf
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc" {
   for_each = var.all_vpcs
 
-  name                    = each.value.name
-  auto_create_subnetworks = each.value.autocreatesubnetworks
-  routing_mode            = each.value.routingmode
+  name                            = each.value.name
+  auto_create_subnetworks         = each.value.autocreatesubnetworks
+  routing_mode                    = each.value.routingmode
   delete_default_routes_on_create = each.value.deletedefaultroutesoncreate
 }
 
 resource "google_compute_subnetwork" "webapp-subnet" {
   for_each = var.all_vpcs
 
-  name          = each.value.webappsubnet
-  network       = google_compute_network.vpc[each.key].self_link
-  ip_cidr_range = each.value.webappsubnetcidr
+  name                     = each.value.webappsubnet
+  network                  = google_compute_network.vpc[each.key].self_link
+  ip_cidr_range            = each.value.webappsubnetcidr
+  private_ip_google_access = each.value.private_ip_google_access
+  depends_on               = [google_compute_network.vpc]
 }
 
 resource "google_compute_subnetwork" "db-subnet" {
-  for_each = var.all_vpcs 
+  for_each = var.all_vpcs
 
   name          = each.value.dbsubnet
   network       = google_compute_network.vpc[each.key].self_link
@@ -33,28 +35,29 @@ resource "google_compute_subnetwork" "db-subnet" {
 resource "google_compute_route" "webapp_route" {
   for_each = var.all_vpcs
 
-  name                = each.value.webapproute
-  network             = google_compute_network.vpc[each.key].self_link
-  dest_range  = each.value.webapproute_dest_range
-  next_hop_gateway    = each.value.webapp_next_hop_gateway
-  tags = ["route-webapp"]
+  name             = each.value.webapproute
+  network          = google_compute_network.vpc[each.key].self_link
+  dest_range       = each.value.webapproute_dest_range
+  next_hop_gateway = each.value.webapp_next_hop_gateway
+  tags             = each.value.webapp_route_tags
+  depends_on       = [google_compute_network.vpc]
 }
 
 resource "google_compute_firewall" "allow_traffic" {
   for_each = var.all_vpcs
 
-  name    = each.value.firewall_allow_name
-  network = google_compute_network.vpc[each.key].self_link
-  priority = 1000
+  name      = each.value.firewall_allow_name
+  network   = google_compute_network.vpc[each.key].self_link
+  priority  = each.value.firewall_allow_priority
   direction = each.value.firewall_allow_direction
   allow {
-    protocol = "tcp"
-    ports = each.value.firewall_allow_ports
+    protocol = each.value.firewall_allow_protocol
+    ports    = each.value.firewall_allow_ports
   }
   source_ranges = each.value.firewall_source_ranges
-  target_tags = ["route-webapp"]
-  depends_on = [google_compute_network.vpc]
-  
+  target_tags   = each.value.firewall_allow_target_tags
+  depends_on    = [google_compute_network.vpc]
+
 }
 
 resource "google_compute_firewall" "deny_ssh_login" {
@@ -62,41 +65,133 @@ resource "google_compute_firewall" "deny_ssh_login" {
 
   name    = each.value.firewall_deny_name
   network = google_compute_network.vpc[each.key].self_link
-  
-   deny {
-    protocol = "tcp"
+
+  deny {
+    protocol = each.value.firewall_allow_protocol
     ports    = each.value.firewall_deny_ports
   }
   source_ranges = each.value.firewall_source_ranges
-  target_tags = ["route-webapp"]
-  depends_on = [google_compute_network.vpc]
-  
+  target_tags   = each.value.firewall_allow_target_tags
+  depends_on    = [google_compute_network.vpc]
+
 }
 
 resource "google_compute_instance" "my-instance" {
   for_each = var.all_vpcs
 
-  name         = "my-instance-${each.key}"
-  machine_type = each.value.vm_machine_type 
+  name         = "${each.value.vm_name}-${each.key}"
+  machine_type = each.value.vm_machine_type
   zone         = each.value.vm_zone
 
   boot_disk {
     initialize_params {
       image = each.value.image
-      size = each.value.size
-      type = each.value.disktype
+      size  = each.value.size
+      type  = each.value.disktype
     }
     mode = each.value.vm_disk_mode
   }
 
-  
   network_interface {
     access_config {
       network_tier = each.value.vm_networking_tier
     }
-   subnetwork = google_compute_subnetwork.webapp-subnet[each.key].id
+    subnetwork = google_compute_subnetwork.webapp-subnet[each.key].id
 
   }
 
-  tags = ["route-webapp"]  
+  tags = each.value.webapp_route_tags
+  //make db IP dynamic
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+
+    echo "spring.datasource.url=jdbc:postgresql://${each.value.cloudsql_private_ip_address}:${each.value.cloudsql_port}/webapp" > /opt/application.properties
+    echo "spring.datasource.username=${google_sql_user.postgres-db-user[each.key].name}" >> /opt/application.properties
+    echo "spring.datasource.password=${google_sql_user.postgres-db-user[each.key].password}" >> /opt/application.properties
+    echo "spring.datasource.hikari.connection-timeout=3000" >> /opt/application.properties
+    echo "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect" >> /opt/application.properties
+    echo "spring.jpa.hibernate.ddl-auto=update" >> /opt/application.properties
+    echo "spring.jpa.show-sql=true" >> /opt/application.properties
+    echo "spring.jpa.properties.hibernate.format_sql=true"
+  
+  EOT
+
+  depends_on = [google_sql_database_instance.postgres-db-instance]
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  for_each = var.all_vpcs
+
+  provider      = google-beta
+  project       = var.project_id
+  name          = each.value.private_ip_name
+  purpose       = each.value.private_ip_purpose
+  address_type  = each.value.private_ip_address_type
+  prefix_length = each.value.private_ip_prefix_length
+  network       = google_compute_network.vpc[each.key].self_link
+}
+
+
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  for_each = var.all_vpcs
+  provider = google-beta
+
+  network = google_compute_network.vpc[each.key].self_link
+  service = each.value.private_vpc_connection_service
+
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address[each.key].name]
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = var.db_name_suffix_length
+}
+
+resource "google_sql_database_instance" "postgres-db-instance" {
+  for_each = var.all_vpcs
+
+  provider            = google-beta
+  project             = var.project_id
+  region              = var.region
+  name                = "${each.value.postgres_db_instance_name}-${random_id.db_name_suffix.hex}"
+  database_version    = each.value.postgres_db_version
+  deletion_protection = each.value.postgres_deletion_protection
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+
+    tier              = each.value.db_tier
+    edition           = each.value.db_edition
+    availability_type = each.value.db_availability_type
+    disk_type         = each.value.db_disk_type
+    disk_size         = each.value.db_disk_size
+    ip_configuration {
+      ipv4_enabled    = each.value.db_instance_ipv4_enabled
+      private_network = google_compute_network.vpc[each.key].self_link
+
+    }
+
+  }
+}
+
+resource "google_sql_database" "postgres-db" {
+  for_each = var.all_vpcs
+  name     = each.value.postgres_db_name
+  instance = google_sql_database_instance.postgres-db-instance[each.key].name
+}
+
+resource "random_password" "password" {
+  length  = 8
+  special = true
+  number  = true
+  upper   = true
+  lower   = true
+}
+
+resource "google_sql_user" "postgres-db-user" {
+  for_each = var.all_vpcs
+  name     = each.value.postgres_db_user_name
+  instance = google_sql_database_instance.postgres-db-instance[each.key].name
+  password = random_password.password.result
 }
--- variables.tf
+++ variables.tf
@@ -2,39 +2,68 @@
 
 variable "project_id" {
   description = "Google Cloud project ID"
-  type = string
+  type        = string
 }
 
 variable "region" {
   description = "GCP region"
-  type = string 
+  type        = string
+}
+
+variable "db_name_suffix_length"{
+  type = number
 }
 
 variable "all_vpcs" {
   type = map(object({
-    name            = string
-    autocreatesubnetworks = bool
-    routingmode = string
-    deletedefaultroutesoncreate = bool 
-    webappsubnet       = string
-    webappsubnetcidr  = string
-    webapproute = string
-    webapproute_dest_range = string
-    webapp_next_hop_gateway = string
-    dbsubnet           = string
-    dbsubnetcidr      = string
-    firewall_allow_name = string
-    firewall_allow_direction = string
-    firewall_allow_ports = list(string)
-    firewall_source_ranges = list(string)
-    firewall_deny_name = string
-    firewall_deny_ports = list(string)
-    image = string
-    size = string
-    disktype = string
-    vm_zone = string
-    vm_machine_type = string
-    vm_networking_tier = string
-    vm_disk_mode = string
+    name                        = string
+    autocreatesubnetworks       = bool
+    routingmode                 = string
+    deletedefaultroutesoncreate = bool
+    webappsubnet                = string
+    webappsubnetcidr            = string
+    private_ip_google_access    = bool
+    webapproute                 = string
+    webapproute_dest_range      = string
+    webapp_next_hop_gateway     = string
+    webapp_route_tags           = list(string)
+    dbsubnet                    = string
+    dbsubnetcidr                = string
+    firewall_allow_name         = string
+    firewall_allow_direction    = string
+    firewall_allow_priority     = number
+    firewall_allow_protocol     = string
+    firewall_allow_ports        = list(string)
+    firewall_allow_target_tags  = list(string)
+    firewall_source_ranges      = list(string)
+    firewall_deny_name          = string
+    firewall_deny_ports         = list(string)
+    image                       = string
+    size                        = string
+    disktype                    = string
+    vm_name                     = string
+    vm_zone                     = string
+    vm_machine_type             = string
+    vm_networking_tier          = string
+    vm_disk_mode                = string
+    private_ip_name             = string
+    private_ip_purpose          = string
+    private_ip_address_type     = string
+    private_ip_prefix_length    = number
+    cloudsql_private_ip_address = string
+    cloudsql_port               = string
+    private_vpc_connection_service = string
+    postgres_db_instance_name   = string
+    postgres_db_version         = string
+    postgres_deletion_protection = bool
+    db_tier                     = string
+    db_edition                 = string
+    db_availability_type        = string
+    db_disk_type                = string
+    db_disk_size               = number
+    db_instance_ipv4_enabled  = bool
+    postgres_db_name          = string
+    postgres_db_user_name = string
+          
   }))
 }
